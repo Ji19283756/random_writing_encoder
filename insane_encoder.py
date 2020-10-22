e37458edab48065c35337a76b5f1211710c233c8
Ji19283756 <67918859+Ji19283756@users.noreply.github.com>
2020-10-22 18:23:26 -0400

    Update and rename WriteVertically.py to insane_encoder.py

--- insane_encoder.py
+++ insane_encoder.py
@@ -1,4 +1,157 @@
+from time import time
 from random import randint, shuffle
+from math import sqrt as root
+
+message = "Somebody once told me the world is gonna roll me I ain't the sharpest tool" \
+          " in the shed She was looking kind of dumb with her finger and her thumb"
+
+
+class Self_Editing_String:
+    def __init__(self, value):
+        self.value = value
+
+    def add(self, new_value):
+        self.value += new_value
+        return self
+
+    def get_len(self):
+        return len(self.value)
+
+    def add_spaces_between_letters(self, spaces=2):
+        self.value = "".join(letter + " " * spaces for letter in self.value)
+        return self
+
+    def make_self_len(self, length):
+        self.value = self.value + " " * (length - len(self.value))
+        return self
+
+
+# TESTS:________________________________________________
+def make_array_v2(message, width=10, height=12, spaces=2):
+    try:
+        if len(message) <= 0 or width < 0 or height < 0:
+            print("something went wrong")
+            return ""
+    except TypeError:
+        print("something went wrong")
+        return ""
+    global total
+    empty_string_array = [Self_Editing_String("") for x in range(height)]
+
+    filled_array = [empty_string_array[x % height].add(letter)
+                    for x, letter in enumerate(message)
+                    if empty_string_array[-1].get_len() < width]
+
+    filled_array = filled_array[:int(root(len(filled_array)))]
+    # filled_array = [filled_array[x] for x in range(int(sqrt(len(filled_array))))]
+
+    spaced_array = [line.add_spaces_between_letters(spaces=spaces) for line in filled_array]
+
+    consistant_array = [line.make_self_len(width) for line in spaced_array]
+
+    printed_string = "".join(line.value + "\n" for line in consistant_array)
+
+    return printed_string
+
+
+# def left_right_horizontal(message, width=10, height=12, spaces=2, start="top"):
+#    width *= (1 + spaces)
+#
+#    message = "".join(letter + " " * spaces for letter in message)
+#
+#    arranged_array = [message[start:start + width]
+#                      for start, x in zip(range(0, len(message), width), range(height))]
+#
+#    consistant_array = [line + " " * (width - len(line)) + "\n"
+#                        for line in arranged_array]
+#
+#    printed_string = "".join(consistant_array)
+#
+#    return printed_string
+
+
+def speed_test():
+    height = 1_000
+    width = 1_000
+
+    all_char = 'abcdefghijklmnopqrstuvwxyz'
+
+    start_time = time()
+    randomized_string = "".join(all_char[randint(0, 25)] for x in range(1_000_000))
+    string_time = time() - start_time
+
+    start_time = time()
+    og = left_right_horizontal(randomized_string, height=height, width=width, spaces=spaces)
+    # print(classless)
+    og_time = time() - start_time
+
+    start_time = time()
+    v2 = left_right_hor_v2(randomized_string, height=height, width=width, spaces=spaces)
+    # print(with_class)
+    v2_time = time() - start_time
+
+    print(
+
+        f"Same result for both: {og == v2}\n"
+        f"Time it took using og: {round(og_time, 5)} sec(s)\n" \
+        f"Time it took using v2 : {round(v2_time, 5)} sec(s)\n" \
+        F"Time it took to make the string: {round(string_time, 5)} sec(s)"
+
+    )
+
+
+# def south_west_diagonal(message, width=12, height=19, spaces=2, start="top-left"):
+#    def len_filler(line):
+#        return (width * (spaces + 1) - len(line))
+#
+#    if not (isinstance(message, str) and isinstance(width, int) \
+#            and isinstance(width, int) and isinstance(width, int)) \
+#            or width < 0 or height < 0 or spaces < 0 or len(message) <= 0:
+#        print("something went wrong")
+#        return ""
+#
+#    empty_array = ["" for x in range(height)]
+#
+#    message_increment = 0
+#
+#    for x in range(1, len(message) + 1):
+#        for y in range(x):
+#            if len(empty_array[y % height]) < width * (1 + spaces):
+#                if start.lower() == "top-left":
+#                    empty_array[y] += message[message_increment] + " " * spaces
+#                else:
+#                    empty_array[y] += " " * spaces + message[message_increment]
+#                message_increment += 1
+#
+#    if not start.lower() == "top-left":
+#        empty_array = [line[::-1] for line in empty_array]
+#
+#    consistant_array = [line + " " * len_filler(line) + "\n" for line in empty_array]
+#
+#    printed_array = "".join(consistant_array)
+#
+#    return printed_array
+#
+#
+# def new_north_east(message, width=12, height=19, spaces=2, start="top-left"):
+#    def len_filler(line):
+#        return (width * (spaces + 1) - len(line))
+#
+#    empty_array = ["" for x in range(height + 1)]
+#
+#    message_increment = 0
+#
+#    for x in range(len(message[:width * height])):
+#        for y in range(x + 1, 0, -1):
+#            if len(empty_array[y % (height + 1)]) < width * (1 + spaces) and y % (height + 1) != 0:
+#                empty_array[y] += message[message_increment] + " " * spaces
+#                message_increment += 1
+#
+#    consistant_array = [line + " " * len_filler(line) + "\n" for line in empty_array]
+#
+#    printed_array = "".join(consistant_array[1::])
+#    return printed_array
+
 
 # OFICIAL_________________________
 def flip_vertically(double_array):
@@ -431,7 +584,7 @@ def BL_south_east_diagonal(message, width=12, height=19, spaces=2):
             bottom = 0
 
         for y in range(x - not_bigger_than_height, bottom, -1):
-            # print(f"inc {message_increment}")
+
             coord = y % (height + 1)
 
             if message_increment > len(message) or message_increment > len(message) - 1:
@@ -442,8 +595,7 @@ def BL_south_east_diagonal(message, width=12, height=19, spaces=2):
                 message_increment += 1
 
         not_bigger_than_height += x > height
-    # for thing in empty_string:
-    #   print(thing)
+
     consistant_array = make_consistant(empty_string, width, spaces)
 
     flipped_array = flip_horizontally(consistant_array)
@@ -462,8 +614,7 @@ def BL_south_east_diagonal_revert(message, spaces=2):
     final_message = []
     message_array = remove_spaces_and_make_double_array(message, spaces)
     message_array = flip_horizontally(message_array)
-    # for line in message_array:
-    #    print(line)
+
     for x in range(len(message), 0, -1):
         thing = x
         while True:
@@ -473,8 +624,7 @@ def BL_south_east_diagonal_revert(message, spaces=2):
                 thing += 1
             except IndexError:
                 break
-    # for line in message_array:
-    #    print(line)
+
     mini_array, increment = 0, 0
 
     while len(message_array[0]) > 0:
@@ -504,7 +654,7 @@ def BR_south_west_diagonal(message, width=12, height=19, spaces=2):
             bottom = 0
 
         for y in range(x - not_bigger_than_height, bottom, -1):
-            # print(f"inc {message_increment}")
+
             coord = y % (height + 1)
 
             if message_increment > len(message) or message_increment > len(message) - 1:
@@ -514,8 +664,7 @@ def BR_south_west_diagonal(message, width=12, height=19, spaces=2):
                 message_increment += 1
 
         not_bigger_than_height += x > height
-    # for thing in empty_string:
-    #   print(thing)
+
     consistant_array = make_consistant(empty_string, width, spaces)
 
     consistant_array = flip_vertically(consistant_array)
@@ -539,8 +688,7 @@ def BR_south_west_diagonal_revert(message, spaces=2):
     message_array = flip_horizontally(message_array)
     message_array = flip_vertically(message_array)
 
-    # for line in message_array:
-    #    print(line)
+
     for x in range(len(message), 0, -1):
         thing = x
         while True:
@@ -550,8 +698,7 @@ def BR_south_west_diagonal_revert(message, spaces=2):
                 thing += 1
             except IndexError:
                 break
-    # for line in message_array:
-    #    print(line)
+
     mini_array, increment = 0, 0
 
     while len(message_array[0]) > 0:
@@ -629,23 +776,17 @@ def TL_south_west_diagonal_revert(message, spaces=2):
             else:
                 increment -= 1
 
-    # for kang in message_array:
-    #    print(kang)
+
     for x in range(len(message_array) - 1, -1, -1):
         for y in range(x, x - width, -1):
 
             try:
-                # increment = y
+
                 final_message += message_array[y][-1]
                 message_array[y].pop(-1)
-                # print(f"succ increment {increment}")
+
             except:
                 break
-
-            # if len(next_array) == len(current_array) + 1 or increment == 0:
-            #    increment -= 1
-            # else:
-            #    print(f"y {increment}")
 
     printed_string = "".join(final_message[::-1])  # .strip()
     return printed_string
@@ -723,17 +864,14 @@ def TR_south_east_diagonal_revert(message, spaces=2):
         for y in range(x, x - width, -1):
 
             try:
-                # increment = y
+
                 final_message += message_array[y][-1]
                 message_array[y].pop(-1)
-                # print(f"succ increment {increment}")
+
             except:
                 break
 
-            # if len(next_array) == len(current_array) + 1 or increment == 0:
-            #    increment -= 1
-            # else:
-            #    print(f"y {increment}")
+
 
     printed_string = "".join(final_message[::-1])  # .strip()
     return printed_string
@@ -800,23 +938,16 @@ def BL_south_west_diagonal_revert(message, spaces=2):
             else:
                 increment -= 1
 
-    # for kang in message_array:
-    #    print(kang)
     for x in range(len(message_array) - 1, -1, -1):
         for y in range(x, x - width, -1):
 
             try:
-                # increment = y
+
                 final_message += message_array[y][-1]
                 message_array[y].pop(-1)
-                # print(f"succ increment {increment}")
+
             except:
                 break
-
-            # if len(next_array) == len(current_array) + 1 or increment == 0:
-            #    increment -= 1
-            # else:
-            #    print(f"y {increment}")
 
     printed_string = "".join(final_message[::-1])  # .strip()
     return printed_string
@@ -892,17 +1023,12 @@ def BR_south_east_diagonal_revert(message, spaces=2):
         for y in range(x, x - width, -1):
 
             try:
-                # increment = y
+
                 final_message += message_array[y][-1]
                 message_array[y].pop(-1)
-                # print(f"succ increment {increment}")
+
             except:
                 break
-
-            # if len(next_array) == len(current_array) + 1 or increment == 0:
-            #    increment -= 1
-            # else:
-            #    print(f"y {increment}")
 
     printed_string = "".join(final_message[::-1])  # .strip()
     return printed_string
@@ -917,7 +1043,7 @@ def make_random_string(length):
 
 
 def the_most_insane_encoder(message, switches, height=10, width=10,
-                            type_of_encoding_key = "random", encoding_key = ""):
+                            type_of_encoding_key="random", encoding_key=""):
     TL_TD = TL_top_down_vertical
     TL_TD_R = TL_top_down_vertical_revert
     TR_TD = TR_top_down_vertical
@@ -951,6 +1077,40 @@ def the_most_insane_encoder(message, switches, height=10, width=10,
     BR_SE = BR_south_east_diagonal
     BR_SE_R = BR_south_east_diagonal_revert
 
+    opposite_dict = \
+        {TL_TD: TL_TD_R,
+         TR_TD: TR_TD_R,
+         BL_TU: BL_TU_R,
+         BR_TU: BR_TU_R,
+         TL_LR: TL_LR_R,
+         BL_LR: BL_LR_R,
+         TR_LR: TR_LR_R,
+         BR_RL: BR_RL_R,
+         TL_NE: TL_NE_R,
+         TR_NW: TR_NW_R,
+         BL_SE: BL_SE_R,
+         BR_SW: BR_SW_R,
+         TL_SW: TL_SW_R,
+         TR_SE: TR_SE_R,
+         BL_SW: BL_SW_R,
+         BR_SE: BR_SE_R,
+         TL_TD_R: TL_TD,
+         TR_TD_R: TR_TD,
+         BL_TU_R: BL_TU,
+         BR_TU_R: BR_TU,
+         TL_LR_R: TL_LR,
+         BL_LR_R: BL_LR,
+         TR_LR_R: TR_LR,
+         BR_RL_R: BR_RL,
+         TL_NE_R: TL_NE,
+         TR_NW_R: TR_NW,
+         BL_SE_R: BL_SE,
+         BR_SW_R: BR_SW,
+         TL_SW_R: TL_SW,
+         TR_SE_R: TR_SE,
+         BL_SW_R: BL_SW,
+         BR_SE_R: BR_SE}
+
     new_encoders = [
         BL_LR,
         BL_SE,
@@ -968,7 +1128,7 @@ def the_most_insane_encoder(message, switches, height=10, width=10,
         TR_NW,
         TR_SE,
         TR_TD
-            ]
+    ]
     new_decoders = [
         BL_LR_R,
         BL_SE_R,
@@ -986,18 +1146,18 @@ def the_most_insane_encoder(message, switches, height=10, width=10,
         TR_NW_R,
         TR_SE_R,
         TR_TD_R
-            ]
+    ]
 
     dict_chars = make_random_string(32)
     first_sixteen = dict_chars[:16]
     last_sixteen = dict_chars[16:]
 
-    encoding_dict ={char:func for char,func in zip(first_sixteen,new_encoders)}
-    decoding_dict= {char:func for char,func in zip(last_sixteen,new_decoders)}
+    encoding_dict = {char: func for char, func in zip(first_sixteen, new_encoders)}
+    decoding_dict = {char: func for char, func in zip(last_sixteen, new_decoders)}
 
     order_of_encoding = "".join(first_sixteen[randint(0, 15)] if x % 2 == 0
-                       else last_sixteen[randint(0, 15)]
-                            for x in range(switches))
+                                else last_sixteen[randint(0, 15)]
+                                for x in range(switches))
     global success
     message = message + " " * (height * width - len(message))
     og_message = message
@@ -1010,10 +1170,10 @@ def the_most_insane_encoder(message, switches, height=10, width=10,
             message = decoding_dict[letter](message, spaces=0)
             # print(f"{decoding_dict[option].__name__}")
 
-    return message,order_of_encoding,dict_chars
+    return message, order_of_encoding, dict_chars
 
 
-def the_most_insane_decoder(message,dict_chars,order_of_encoding,height=10,width=10):
+def the_most_insane_decoder(message, dict_chars, order_of_encoding, height=10, width=10):
     TL_TD = TL_top_down_vertical
     TL_TD_R = TL_top_down_vertical_revert
     TR_TD = TR_top_down_vertical
@@ -1130,26 +1290,84 @@ def the_most_insane_decoder(message,dict_chars,order_of_encoding,height=10,width
         if x % 2 == result:
             message = opposite_dict[encoding_dict[letter]](message, spaces=0)
         else:
-            message = opposite_dict[decoding_dict[letter]]\
+            message = opposite_dict[decoding_dict[letter]] \
                 (message, height=height, width=width, spaces=0)
 
     return message
+
+
+def descramble_readable_message(message):
+    replace_pairs = ["<",">","\"|","|\""]
+
+    for look_for in replace_pairs:
+        print(f"replacing {look_for} with {'(())'}")
+        message = message.replace(look_for,"(())")
+    message = [line for line in message.split("(())") if len(line) > 0]
+    for x, value in enumerate(message):
+        if x % 2 == 0:
+            message[x] = int(value)
+    message = "".join(value * " " if isinstance(value, int) else value for value in message)
+
+    return message
+
+
+def make_scrambled_message_readable(message):
+    def make_num_x_long(number, length):
+        return (length - len(str(number))) * "0" + str(number)
+
+    largest = float("-inf")
+    count = 0
+    print(f"there are {message.count(' ')} spaces in the message")
+    for letter in message:
+        if letter == " ":
+            count += 1
+        else:
+            if count > largest:
+                largest = count
+            count = 0
+    longest_digit = len(str(largest))
+
+    count = 0
+    readable_message = []
+    for letter in message:
+        if letter == " ":
+            count += 1
+        else:
+            readable_message += [f"<{str(make_num_x_long(count, longest_digit))}>|\"{letter}\"|"]
+            count = 0
+
+    readable_message += [f"<{str(make_num_x_long(count, longest_digit))}>"]
+
+    readable_message = ''.join(readable_message)
+
+    return readable_message
+
+
+
 # OPTIONS:_______________________________________________________________________
-message = "Nobody is going to ever see this code but im fine with that"
+message = "Somebody once told me the world was gonna roll me I ain't the sharpest tool" \
+          " in the shed She was looking kind of dumb with her finger and her thumb"
 success = 0
-height = 11
-width = 11
+height = 1_00
+width = 1_00
+
 spaces = 2
-switch_amount=10
-encoded_messages=[]
-average=[]
+switch_amount = 1_000
+#encoded_messages = []
+#average = []
 # _______________________________________________________________________________
-encoded_message,encoding_list,thirty_two_chars = the_most_insane_encoder\
-    (message, switch_amount,height=height,width=width)
-print(f"scrambled message : {encoded_message}"
-      f"order of encoding : {encoding_list}"
-      f"dict chars : {thirty_two_chars}")
-decoded_message= the_most_insane_decoder\
-    (encoded_message,thirty_two_chars,encoding_list,height=height,width=width)
-print(f"decoded message : {decoded_message}")
+encoded_message, encoding_list, thirty_two_chars = the_most_insane_encoder \
+    (message, switch_amount, height=height, width=width)
+encoded_message = make_scrambled_message_readable(encoded_message)
+
+print(f"scrambled message : \n{encoded_message}"
+      f"\norder of encoding : {encoding_list}"
+      f"\ndict chars : {thirty_two_chars}")
+
+encoded_message = descramble_readable_message(encoded_message)
+decoded_message = the_most_insane_decoder \
+    (encoded_message, thirty_two_chars, encoding_list, height=height, width=width)
+
+print(f"decoded message : {decoded_message.strip()}")
+
 
